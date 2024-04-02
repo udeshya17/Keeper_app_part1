933d350a58f329295c8dccd978d580329f08d4f9
Udeshya Sharma <114618734+udeshya17@users.noreply.github.com>
2024-04-03 02:30:27 +0530

    Initial commit

---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>This is Note Title</h1>
+      <p>This is Note Content</p>
+    </div>
+  );
+}
+
+export default Note;
